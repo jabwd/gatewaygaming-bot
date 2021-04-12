67b79e75ac3d57d07d774622c8b570fe7fb40b8d
Antwan van Houdt <jabwd@exurion.com>
2021-04-12 03:35:32 +0200

    connection to discord

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,93 @@
-extern crate dotenv;
+use std::{
+    collections::HashSet,
+    env,
+    sync::Arc,
+};
+use serenity::{
+    async_trait,
+    client::bridge::gateway::ShardManager,
+    framework::{
+        StandardFramework,
+        standard::macros::group,
+    },
+    http::Http,
+    model::{event::ResumedEvent, gateway::Ready, channel::Message},
+    prelude::*
+};
 
 mod commands;
 
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct Handler;
+
+#[group]
+struct General;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("Gateway bot is ready {:?}", ready.user.name);
+    }
+
+    async fn message(&self, ctx: Context, msg: Message) {
+        println!("Message received: {:?}", msg);
+    }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        println!("Resumed");
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
-    println!("=> Starting Gateway bot");
-    println!("Hello, world!");
+    dotenv::dotenv().expect("Failed to load environment variables from .env file");
 
     let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a discord token defined in the environment");
-    
+        .expect("Expected a discord authentication token in the environment");
+
+    let http = Http::new_with_token(&token);
+
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+            (owners, info.id)
+        },
+        Err(reason) => panic!("Could not load discord bot info: {:?}", reason),
+    };
+
+    // Create the framework
     let framework = StandardFramework::new()
         .configure(|c| c
-            .prefix("g/"));
+            .owners(owners)
+            .prefix("-"))
+        .group(&GENERAL_GROUP);
+    
+    let mut client = Client::builder(&token)
+            .framework(framework)
+            .event_handler(Handler)
+            .await
+            .expect("Err creating client");
+
+    
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register SIGKILL handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(reason) = client.start().await {
+        println!("Client error: {:?}", reason);
+    }
 }
